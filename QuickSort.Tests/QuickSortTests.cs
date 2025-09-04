b419d3ebca8d11d027ac6c2c99fdc0efa0faec27
Gavin Chin <1640786+slowmonkey@users.noreply.github.com>
2025-09-04 21:34:26 +0800

    Initial files for quicksort learnings

---
+++ QuickSort.Tests/QuickSortTests.cs
@@ -0,0 +1,18 @@
+﻿namespace QuickSort.Tests;
+
+public class QuickSortTests
+{
+    [Theory]
+    [InlineData(new int[] { 5, 2, 9, 1, 7 }, new int[] { 1, 2, 5, 7, 9 })]
+    [InlineData(new int[] { -1, 1, 1, 2, 9, 5, 1, 7 }, new int[] { -1, 1, 1, 1, 2, 5, 7, 9 })]
+    [InlineData(new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })]
+    [InlineData(new int[] { 42, 7, 19, 3, 88, 54, 23, 16, 71, 5, 30, 12, 91, 2, 67, 49, 81, 14, 60, 35 }, new int[] {2, 3, 5, 7, 12, 14, 16, 19, 23, 30, 35, 42, 49, 54, 60, 67, 71, 81, 88, 91})]
+    public void QuickSort_UnsortedItems_SortedItemsExpected(int[] items, int[] expectedResult)
+{
+    Console.WriteLine("Before: " + string.Join(", ", items));
+    QuickSortLearnings.QuickSort(items, 0, items.Length - 1);
+    Console.WriteLine("After: " + string.Join(", ", items));
+
+    Assert.Equal(expectedResult, items);
+}
+}
---
+++ QuickSort/QuickSortLearnings.cs
@@ -0,0 +1,68 @@
+﻿namespace QuickSort;
+
+public class QuickSortLearnings
+{
+    public static void QuickSort(int[] items, int leftIndex, int rightIndex)
+    {
+        Console.WriteLine($"QuickSort: items: {string.Join(", ", items)}, leftIndex: {leftIndex}, rightIndex: {rightIndex}");
+
+        if (leftIndex < rightIndex)
+        {
+            int pivotIndex = Partition(items, leftIndex, rightIndex);
+
+            // Sort the left side
+
+            Console.WriteLine($"QuickSort: Sort Left Side of {items[pivotIndex]}");
+
+            QuickSort(items, leftIndex, pivotIndex - 1);
+
+            // Sort the right side
+
+            Console.WriteLine($"QuickSort: Sort Right Side of {items[pivotIndex]}");
+
+            QuickSort(items, pivotIndex + 1, rightIndex);
+        }
+    }
+
+    public static int Partition(int[] items, int leftIndex, int rightIndex)
+    {
+        // Taking the right index value as the pivot number.
+
+        int pivot = items[rightIndex];
+
+        Console.WriteLine($"QuickSort: pivot value: {pivot}");
+
+        int index = leftIndex - 1;
+
+        for (int counter = leftIndex; counter <= rightIndex - 1; counter++)
+        {
+            Console.WriteLine($"Compare: {items[counter]} with Pivot: {pivot}");
+
+            if (items[counter] < pivot)
+            {
+                index++;
+
+                Console.WriteLine($"Swap: {items[index]} at {index} with {items[counter]} at {counter}");
+
+                Swap(items, index, counter);
+
+            }
+        }
+
+        Console.WriteLine($"Swap: {items[index + 1]} at {index + 1} with {items[rightIndex]} at {rightIndex}");
+
+        Swap(items, index + 1, rightIndex);
+        
+        return index + 1;
+    }
+
+    public static void Swap(int[] items, int leftIndex, int rightIndex)
+    {
+
+        int temp = items[leftIndex];
+        items[leftIndex] = items[rightIndex];
+        items[rightIndex] = temp;
+
+        Console.WriteLine("Now: " + string.Join(", ", items));
+    }
+}
